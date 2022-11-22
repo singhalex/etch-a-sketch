948881deceb550920aa8f644aaf6d2fb16318be3
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-22 15:08:25 -0800

    Add button to make rainbow colors
    When pressed, the button changes the pixels to a random RGB color when
    moused over.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,66 @@
 let gridSize = 16;
+let flexBasis;
 const reset = document.querySelector("#reset");
 const container = document.querySelector("#container");
+const setGridSize = document.querySelector("#set-grid");
+const rainbow = document.querySelector("#rainbow");
+
+function setFlexBasis(gridSize) {
+    flexBasis = 1 / gridSize * 100;
+}
 
 function createPixel() {
     const pixel = document.createElement("div");
     pixel.className = "pixel";
+    setFlexBasis(gridSize)
+    pixel.style.flexBasis = `${flexBasis}%`;
     pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "#FF9F9F";
+        pixel.style.backgroundColor = "#FF9F9F";        
     })
     container.appendChild(pixel);
 }
 
-for (let i = 1; i <= (gridSize * gridSize); i++) {
+function createGrid() {
+    for (let i = 1; i <= (gridSize * gridSize); i++) {
     createPixel();
+    }
 }
 
-const pixel = document.querySelectorAll(".pixel");
+function removeGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+    for (let i = 0; i < pixels.length; i++) {
+        container.removeChild(pixels[i]);
+    }
+}
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+console.log(randomColor())
+createGrid();
 
 reset.addEventListener("click", () => {
-    for (let i = 0; i < pixel.length; i++) {
-        pixel[i].style.backgroundColor = "#FFFAD7";
+    const pixels = document.querySelectorAll(".pixel");
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].style.backgroundColor = "#FFFAD7";
+    }
+})
+
+setGridSize.addEventListener("click", () => {
+    gridSize = prompt("Enter the grid size:");
+    removeGrid();
+    createGrid();
+})
+
+rainbow.addEventListener("click", () => {
+    const pixels = document.querySelectorAll(".pixel");
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener("mouseover", () => {
+            pixels[i].style.backgroundColor = randomColor();
+        })
     }
 })
