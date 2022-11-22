33d0ade6b438dc42603f34b80a46e15795e2d6bd
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-21 22:03:25 -0800

    Add reset button
    When pressed, reset button returns the grid to the original background
    color.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let gridSize = 16;
-
+const reset = document.querySelector("#reset");
 const container = document.querySelector("#container");
 
 function createPixel() {
@@ -15,4 +15,10 @@ for (let i = 1; i <= (gridSize * gridSize); i++) {
     createPixel();
 }
 
-const pixel = document.querySelector(".pixel");
+const pixel = document.querySelectorAll(".pixel");
+
+reset.addEventListener("click", () => {
+    for (let i = 0; i < pixel.length; i++) {
+        pixel[i].style.backgroundColor = "#FFFAD7";
+    }
+})
